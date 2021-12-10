041315f3b97df058cb74b8407f02cd944c91e8eb
Raksu25 <90251072+Raksu25@users.noreply.github.com>
2021-12-10 08:45:30 +0200

    Create checktemp.js

---
+++ checktemp.js
@@ -0,0 +1,20 @@
+
+
+check temp
+
+function checkTemp (temp) {
+    console.log("Temp on "+temp)
+    
+    if (temp <21) {
+        console.log("liiga külm")
+        return -1
+    }
+    else if (temp <= 40) {
+        console.log("paras temperatuur")
+        return 0;
+    }
+    else {
+        console.log("liiga kuum")
+        return 1
+    }
+}
